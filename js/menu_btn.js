374f0040d4f16481b87db8cd5107f943c89892d3
Alina <alinka11za@gmail.com>
2022-08-08 10:39:12 +0000

    changed js for btn up

--- js/menu_btn.js
+++ js/menu_btn.js
@@ -1,12 +1,14 @@
-var t;
-function up() {
-  var top = Math.max(
-    document.body.scrollTop,
-    document.documentElement.scrollTop
-  );
-  if (top > 0) {
-    window.scrollBy(0, -2500);
-    t = setTimeout("up()", 500);
-  } else clearTimeout(t);
-  return false;
-}
+(function () {
+  let timeoutId;
+  function up() {
+    let top = Math.max(
+      document.body.scrollTop,
+      document.documentElement.scrollTop
+    );
+    if (top > 0) {
+      window.scrollBy(0, -2500);
+      timeoutId = setTimeout("up()", 500);
+    } else clearTimeout(timeoutId);
+    return false;
+  }
+})();
